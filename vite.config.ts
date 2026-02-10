87c6a7169a0820c2a7fedaabb35b6142a35f59d0
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-02-10 17:41:25 -0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,24 @@
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [tailwindcss(), sveltekit()],
+    server: {
+      port: parseInt(env.PORT) || 5173,
+      strictPort: true,
+    }
+  };
+});
+
+
+
+
+// import tailwindcss from '@tailwindcss/vite';
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
