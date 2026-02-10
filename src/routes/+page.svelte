e581078e4425b9e50edafb06ae839e47e85a4e9c
Alex Robinson <alexrrobinson05@gmail.com>
2026-02-10 09:05:25 -0600

    fix master

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import About from "$lib/about.svelte";
     import Projects from "$lib/projects.svelte";
     import Top from "$lib/top.svelte";
-    import Navbar from "$lib/nav.svelte";
+    import Navbar from "$lib/components/navbar.svelte";
     import Projectssection from "$lib/projects/projectssection.svelte";
 </script>
 
