98ba31f1971282e4d2dc09de8e3845bf758708a3
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-03-10 14:31:36 -0500

    Updated dependicies

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,23 +2,18 @@ module.exports = {
   apps: [
     {
       name: 'adv-sv-app',
-      // We use 'bun' as the primary command
       script: 'bun',
-      // We use 'run' to execute the SvelteKit entry point
       args: 'run /home/arobinson/dev/projects/arrobinson-sveleteapp-v2/build/index.js',
       cwd: '/home/arobinson/dev/projects/arrobinson-sveleteapp-v2',
       env: {
         NODE_ENV: 'production',
         PORT: 7085,
-        // Using 0.0.0.0 prevents the "IP not ready" reboot crash
         HOST: '0.0.0.0', 
         ORIGIN: 'https://alexdoesvoices.com',
         ADDRESS_HEADER: 'x-forwarded-for',
         PROTOCOL_HEADER: 'x-forwarded-proto'
       },
-      // Safety delay to ensure the OS is ready after a reboot
       restart_delay: 5000,
-      // Prevents Bun from using too much RAM and crashing the server
       max_memory_restart: '500M'
     }
   ]
