c54e8d81a75395b325e218d9ca281cd320877ece
Alex Robinson <alexrrobinson05@gmail.com>
2026-04-20 18:36:24 -0500

    added recs page

---
+++ src/routes/recs/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+
+<section id="recs">
+<div class="rec-list">
+    <ul class="software"> 
+        <h3>Here are software that I recomend:</h3>
+        <li></li>
+    </ul>
+</div>
+</section>
+
+
+<style>
+ul.software {
+    h1,h2,h3 {
+    color: whitesmoke;
+    }
+}
+</style>
