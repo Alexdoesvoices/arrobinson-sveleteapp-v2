d52e0e34b3409e89421cf63318253ec44bbfb89a
Alex Robinson <alexrrobinson05@gmail.com>
2026-04-21 13:38:05 -0500

    cleaned up alot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script>
-    import About from "$lib/about.svelte";
-    import Projects from "$lib/projects/projects.svelte";
-    import Top from "$lib/top.svelte";
-    import Navbar from "$lib/components/navbar.svelte";
+    import About from "$lib/sections/about.svelte";
+    import Projects from "$lib/sections/projects.svelte";
+    import Top from "$lib/sections/top.svelte";
 </script>
 
 <main>
-    <!-- <Navbar /> -->
     <Top />
-    <!-- <About /> -->
     <Projects />
 </main>
-
-
-<style>
-
-</style>
